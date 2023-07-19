1191d390566e70b9156ce3c7941a33a35a8f0790
Bruce Webster <brucewebster222@gmail.com>
2023-07-19 16:36:54 -0400

    Removed unecessary <></>

--- src/ExperienceItem.jsx
+++ src/ExperienceItem.jsx
@@ -1,13 +1,11 @@
 export default function ExperienceItem({ title, subTitle, description }) {
   return (
-    <>
-      <div className="experience-container">
-        <div className="flex">
-          <h3>{title.toUpperCase()}</h3>
-          <h4>{subTitle}</h4>
-        </div>
-        <p>{description}</p>
+    <div className="experience-container">
+      <div className="flex">
+        <h3>{title.toUpperCase()}</h3>
+        <h4>{subTitle}</h4>
       </div>
-    </>
+      <p>{description}</p>
+    </div>
   );
 }
