915640b8fe547804df5c7aba5cb47858875529b0
Bruce Webster <brucewebster222@gmail.com>
2023-09-27 11:10:40 -0400

    Added information to job experience

--- src/ExperienceList.jsx
+++ src/ExperienceList.jsx
@@ -5,15 +5,27 @@ export default function ExperienceList() {
   const experiences = [
     {
       id: uuidv4(),
-      title: "English Teacher",
-      subTitle: "2019 - 2020",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      title: "Language Teacher",
+      subTitle:
+        "Academia de lenguas (2019 - 2020)" +
+        <br /> +
+        "Sapienza Academy (2019 - 2020)",
+      description:
+        "I taught English, Spanish, and Italian in the Dominican Republic, providing language education in this diverse and vibrant country.",
     },
     {
       id: uuidv4(),
-      title: "English Teacher",
-      subTitle: "2020 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      title: "Sapienza Academy",
+      subTitle: "Language Teacher (2020 - 2023)",
+      description:
+        "I taught English, Spanish, and Italian in the Dominican Republic, providing language education in this diverse and vibrant country.",
+    },
+    {
+      id: uuidv4(),
+      title: "Sapienza Academy",
+      subTitle: "Language Teacher (2020 - 2023)",
+      description:
+        "I taught English, Spanish, and Italian in the Dominican Republic, providing language education in this diverse and vibrant country.",
     },
   ];
 
